2d86db14b8da6de0493564f0b45ded4800720f5b
Maikel Mardjan <nocomplexity@@users.noreply.github.com>
2021-01-06 13:02:24 +0100

    removed line source_parser, not needed anymore...already present in this env

--- conf.py
+++ conf.py
@@ -45,11 +45,12 @@ edit_on_github_branch = 'master'
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates','_templates/sphinx_book_theme']
 
-# To make md files possible in Sphinx only build, so without Jupyter Book
-from recommonmark.parser import CommonMarkParser
-source_parsers = {
-    '.md': CommonMarkParser,
-}
+# # To make md files possible in Sphinx only build, so without Jupyter Book
+# from recommonmark.parser import CommonMarkParser
+
+# source_parsers = {
+#     '.md': CommonMarkParser,
+# }
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
